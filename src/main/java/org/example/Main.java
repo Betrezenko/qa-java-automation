94f733b4706ad1e63881bd59ac618dc78bf2093b
Viktor.Igumenov <betrezenko@gmail.com>
2023-02-02 12:57:25 +0700

    Working with additional windows

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,7 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -12,6 +9,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Set;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.alertIsPresent;
 
@@ -21,37 +19,31 @@ public class Main {
         WebDriver driver = new ChromeDriver();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-
+        JavascriptExecutor js = (JavascriptExecutor)driver;
 
         try {
             driver.get("http://127.0.0.1:5500/index.html");
             Thread.sleep(5000);
 
-            WebElement element1 = driver.findElement(By.id("a")); // alert
-            WebElement element2 = driver.findElement(By.id("b")); // prompt
-            WebElement element3 = driver.findElement(By.id("c")); // confirm
+            String window1 = driver.getWindowHandle(); // get active window id
+            js.executeScript("window.open()"); // use js to open new window
 
-            // click on Link 1 to call alert
-            element1.click();
-            // Alert alert = driver.switchTo().alert(); -- don't know alert is showed or not
-            Alert alert = wait.until(alertIsPresent());
-            alert.accept();
+            Set<String> currentWindows = driver.getWindowHandles(); // create opened windows array
 
-            // click on Link 2 to call prompt, send text and accept
-            element2.click();
-            Alert prompt = wait.until(alertIsPresent());
-            prompt.sendKeys("Super!");
-            prompt.accept();
+            String window2 = null; // create new window variable and set value to null for writing id
 
-            Alert alert2 = wait.until(alertIsPresent());
-            Thread.sleep(2000);
-            alert2.accept();
+            for (String window : currentWindows) {
+                if (!window.equals(window1)) {
+                    window2 = window;
+                    break;
+                }
+            }
 
-            // click on Link 3 to call confirm window
-            element3.click();
-            Alert confirm = wait.until(alertIsPresent());
-            confirm.dismiss();
-
+            driver.switchTo().window(window2);
+            driver.get("http://127.0.0.1:5500/index.html");
+            Thread.sleep(5000);
+            driver.close();
+            driver.switchTo().window(window1);
 
         } catch (InterruptedException e) {
             e.printStackTrace();
