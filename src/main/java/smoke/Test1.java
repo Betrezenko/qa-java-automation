8bb1aaaa57878ec0fa0adb8fcda16c711e03b39d
Viktor.Igumenov <betrezenko@gmail.com>
2023-02-05 22:16:27 +0700

    Test retry

--- src/main/java/smoke/Test1.java
+++ src/main/java/smoke/Test1.java
@@ -1,18 +1,19 @@
 package smoke;
 
+import org.testng.Assert;
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Test1 {
 
-    @Test
+    @Test (retryAnalyzer = Retry.class)
     public void test1() {
         System.out.println("test 1 ");
+        Assert.fail();
     }
 
-    @Test (dependsOnMethods = {"test1"}, alwaysRun = true)
-    public void test11() {
-        System.out.println("test 11 ");
-    }
 }
+
